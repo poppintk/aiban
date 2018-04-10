f54febd5d42da313205ccb078d5efcc49592b646
ATKIN\RYou <RYou@BroadViewSoftware.com>
2018-04-10 17:51:07 -0400

    upload

--- src/App.js
+++ src/App.js
@@ -1,58 +1,81 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Top from './Header_new';
 import header_image from './image/header.png';
-import {
-  Layout,
-  Select 
-} from 'antd';
-const {
-  Header,
-  Footer,
-  Sider,
-  Content
-} = Layout;
+import { Layout, Select } from 'antd';
+import Main from './content/Main';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import AboutUs from './content/Aboutus';
+const { Header, Footer, Sider, Content } = Layout;
 const Option = Select.Option;
 
 export default class App extends Component {
   handleChange(value) {
-    console.log(`selected ${value}`);
+    window.open(value, '_blank');
   }
 
   render() {
-    return ( 
-    <Layout >
-      <Top / >
-      <Content style={{height:'80vh',overflow:'auto'}}> 
-        <img 
-          style = {{
-            width: '90%',
-            display: 'block',
-            margin: '0 auto',
-            height:'200px'
-          }}
-          src = {header_image}/>
-      </Content >
-      <Footer style={{height:'calc(20vh - 117px)',backgroundColor:'#3c56a3'}}>
-      <div style={{color:'white'}}>
-        <span >版权所有：湖南爱邦正明环保工程公司</span>  
-        <span style={{marginLeft:'10px'}}>企业邮箱</span>       
-        <span style={{marginLeft:'10px'}}>网站地图 </span>      
-        <span style={{marginLeft:'10px'}}>技术支持</span>       
-        <span style={{marginLeft:'10px'}}>天气预报</span>       
-        <span style={{marginLeft:'10px'}}>联系我们</span>    
-        <span style={{marginLeft:'10px'}}>友情链接：</span>
-        <Select defaultValue="lucy" style={{ width: 120 }} onChange={this.handleChange} size={'small'}>
-          <Option value="jack">Jack</Option>
-          <Option value="lucy">Lucy</Option>
-          <Option value="disabled" disabled>Disabled</Option>
-          <Option value="Yiminghe">yiminghe</Option>
-        </Select>
-      </div>
-      </Footer> 
-    </Layout >
+    return (
+      <Layout>
+        <Top />
+        <Content style={{ height: '80vh', overflow: 'auto' }}>
+          <img
+            style={{
+              width: '90%',
+              display: 'block',
+              margin: '0 auto',
+              height: '200px'
+            }}
+            src={header_image}
+          />
+
+          <Route exact path="/" component={Main} />
+          <Route path="/about" component={AboutUs} />
+        </Content>
+        <Footer
+          style={{ height: 'calc(20vh - 117px)', backgroundColor: '#3c56a3' }}
+        >
+          <div style={{ color: 'white' }}>
+            <span style={{ marginLeft: '20%' }}>
+              版权所有：湖南爱邦正明环保工程公司
+            </span>
+            <span style={{ marginLeft: '10px' }}>
+              <a
+                href="mailto:1317426180@qq.com"
+                style={{ color: 'white', textDecoration: 'none' }}
+              >
+                企业邮箱
+              </a>
+            </span>
+            <span style={{ marginLeft: '10px' }}>网站地图 </span>
+            <span style={{ marginLeft: '10px' }}>技术支持</span>
+            <span style={{ marginLeft: '10px' }}>天气预报</span>
+            <span style={{ marginLeft: '10px' }}>联系我们</span>
+            <span style={{ marginLeft: '10px' }}>友情链接：</span>
+            <Select
+              defaultValue="国际环保城"
+              style={{ width: 120 }}
+              onChange={this.handleChange}
+              size={'small'}
+            >
+              <Option value="http://www.iepz.cc/">国际环保城</Option>
+              <Option value="http://green.sina.com.cn/">新浪环保</Option>
+              <Option value="http://www.dowater.com/">
+                中国污水处理工程网
+              </Option>
+              <Option value="http://www.cenews.com.cn/">中国环境网</Option>
+              <Option value="http://www.chinaenvironment.com/">
+                中国环保网
+              </Option>
+              <Option value="http://www.zhb.gov.cn/">中国环保部</Option>
+              <Option value="http://www.epday.com/">中国环境保护网</Option>
+              <Option value="http://www.caepi.org.cn/">
+                中国环保保护产业协会
+              </Option>
+            </Select>
+          </div>
+        </Footer>
+      </Layout>
     );
   }
 }
--- src/Header_new.js
+++ src/Header_new.js
@@ -1,141 +1,195 @@
 import React, { Component } from 'react';
-import {
-    Layout
-} from 'antd';
+import { Layout } from 'antd';
 import logo from './image/logo.png';
-import { Menu, Icon,Input } from 'antd';
+import { Menu, Icon, Input } from 'antd';
+import { Link } from 'react-router-dom';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 const Search = Input.Search;
-const {
-    Header,
-    Footer,
-    Sider,
-    Content
-} = Layout;
+const { Header, Footer, Sider, Content } = Layout;
 
+export default class Top extends Component {
+  state = {
+    is_inline: false
+  };
 
-export default class Top extends Component{
-    state = {
-        is_inline: false
+  handleResize = () => {
+    console.log(window.innerWidth);
+    if (window.innerWidth < 1080) {
+      console.log(true);
+      this.setState({ is_inline: true });
+    } else {
+      console.log(false);
+      this.setState({ is_inline: false });
     }
+  };
 
-    handleResize = () => {
-        console.log(window.innerWidth)
-        if(window.innerWidth < 1080){
-            console.log(true)
-            this.setState({is_inline:true});
-        }else{
-            console.log(false)
-            this.setState({is_inline:false});
-        }
-    }
+  componentDidMount() {
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize);
+  }
 
-    componentDidMount() {
-        this.handleResize();
-        window.addEventListener('resize', this.handleResize)
-      }
-    
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.handleResize)
-    }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
 
-    render(){
-        return ( 
-            <Header 
-                style = {{
-                    backgroundColor: '#adccff',
-                    height: '117px'
-                }}>
-                <div style={{display:'flex',marginTop:'10px'}}> 
-                    <div>
-                        <img src={logo} style={{width:'50px'}}/>
-                    </div>
-                    <div style={{
-                        position:'relative',
-                        top:'-10px',
-                        fontSize:'20px',
-                        marginLeft:'20px'
-                        }}>
-                        爱邦正明
-                    </div>
-                    <div 
-                        style={{
-                            position:'relative',
-                            top:'15px',
-                            left:'-100px'
-                        }}>
-                        ai bang zheng ming
-                    </div>
-                </div>
-                <div style={{position:'absolute',right:'5px',top:'10px' }}>
-                    <Search
-                        placeholder="input search text"
-                        onSearch={value => console.log(value)}
-                        style={{ width: 100}}
-                    />
-                    <span>简体{' '}</span>
-                    <span>繁体 {' '}</span>
-                    <span>English</span>
-                </div>
-                <Menu
-                    onClick={this.handleClick}
-                    selectedKeys={[state.current]}
-                    mode={this.state.is_inline?"inline":"horizontal"}
-                    style={{backgroundColor:'#adccff',position:'absolute',right:'0px',top:'70px'}}
-                >
-                    <SubMenu title={<span><Icon type="mail" key="mail"/>首頁</span>}>
-                        <Menu.Item key="setting:1">關於我們</Menu.Item>
-                        <Menu.Item key="setting:2">新聞中心</Menu.Item>
-                        <Menu.Item key="setting:3">工程業績</Menu.Item>
-                        <Menu.Item key="setting:4">技術研發</Menu.Item>
-                    </SubMenu>
-                    <SubMenu title={<span><Icon type="appstore" key="app"/>關於我們</span>}>
-                        <Menu.Item key="setting:1">企業簡介</Menu.Item>
-                        <Menu.Item key="setting:2">企業文化</Menu.Item>
-                        <Menu.Item key="setting:3">紙質榮譽</Menu.Item>
-                    </SubMenu>
-                    <SubMenu title={<span><Icon type="setting" />新聞中心</span>}>
-                        <Menu.Item key="setting:1">公司新聞</Menu.Item>
-                        <Menu.Item key="setting:2">工程動態</Menu.Item>
-                        <Menu.Item key="setting:3">行業新聞</Menu.Item>
-                    </SubMenu>
-                    <SubMenu title={<span><Icon type="setting" />工程業績</span>}>
-                        <Menu.Item key="setting:1">華東地區</Menu.Item>
-                        <Menu.Item key="setting:2">華南地區</Menu.Item>
-                        <Menu.Item key="setting:3">華中地區</Menu.Item>
-                        <Menu.Item key="setting:4">西南地區</Menu.Item>
-                        <Menu.Item key="setting:5">華北地區</Menu.Item>
-                    </SubMenu>
-                    <SubMenu title={<span><Icon type="setting" />技術研發</span>}>
-                        <Menu.Item key="setting:1">核心技術</Menu.Item>
-                        <Menu.Item key="setting:2">知識產權</Menu.Item>
-                        <Menu.Item key="setting:3">產學研合作</Menu.Item>
-                    </SubMenu>
-                    <SubMenu title={<span><Icon type="setting" />交流合作</span>}>
-                        <Menu.Item key="setting:1">大陸合作</Menu.Item>
-                        <Menu.Item key="setting:2">港臺合作</Menu.Item>
-                        <Menu.Item key="setting:3">國際合作</Menu.Item>
-                    </SubMenu>
-                    <SubMenu title={<span><Icon type="setting" />招賢納士</span>}>
-                        <Menu.Item key="setting:1">領軍人才</Menu.Item>
-                        <Menu.Item key="setting:2">招賢納士</Menu.Item>
-                    </SubMenu>
-                    <SubMenu title={<span><Icon type="setting" />聯係我們</span>}>
-                        <Menu.Item key="setting:1">聯係我們</Menu.Item>
-                    </SubMenu>
-                    <SubMenu title={<span><Icon type="setting" />新聞中心</span>}>
-                        <Menu.Item key="setting:1">Option 1</Menu.Item>
-                        <Menu.Item key="setting:2">Option 2</Menu.Item>
-                    </SubMenu>
-                </Menu>
-            </Header>
-        );
-    }
-    
+  render() {
+    return (
+      <Header
+        style={{
+          backgroundColor: '#adccff',
+          height: '117px'
+        }}
+      >
+        <div style={{ display: 'flex', marginTop: '10px' }}>
+          <div>
+            <img src={logo} style={{ width: '50px' }} />
+          </div>
+          <div
+            style={{
+              position: 'relative',
+              top: '-10px',
+              fontSize: '20px',
+              marginLeft: '20px',
+              fontWeight: 'bold'
+            }}
+          >
+            爱邦正明
+          </div>
+          <div
+            style={{
+              position: 'relative',
+              top: '15px',
+              left: '-100px',
+              fontWeight: 'bold'
+            }}
+          >
+            ai bang zheng ming
+          </div>
+        </div>
+        <div style={{ position: 'absolute', right: '40px', top: '10px' }}>
+          <Search
+            placeholder="查詢"
+            onSearch={value => console.log(value)}
+            style={{ width: 130 }}
+          />
+          <span style={{ marginLeft: '5px' }}>简体/</span>
+          <span>繁体/</span>
+          <span>English</span>
+        </div>
+
+        <Menu
+          onClick={this.handleClick}
+          selectedKeys={[state.current]}
+          mode={this.state.is_inline ? 'inline' : 'horizontal'}
+          style={{
+            backgroundColor: '#adccff',
+            position: 'absolute',
+            right: '0px',
+            top: '70px'
+          }}
+        >
+          <Menu.Item key="setting:1">
+            <Link to="/">
+              <Icon type="mail" key="mail" />首頁
+            </Link>
+          </Menu.Item>
+          <SubMenu
+            title={
+              <span>
+                <Link to="/about">
+                  <Icon type="appstore" key="app" />關於我們
+                </Link>
+              </span>
+            }
+          >
+            <Menu.Item key="setting:1">
+              <Link to="/about">企業簡介</Link>
+            </Menu.Item>
+            <Menu.Item key="setting:2">企業文化</Menu.Item>
+            <Menu.Item key="setting:3">紙質榮譽</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="setting" />新聞中心
+              </span>
+            }
+          >
+            <Menu.Item key="setting:1">公司新聞</Menu.Item>
+            <Menu.Item key="setting:2">工程動態</Menu.Item>
+            <Menu.Item key="setting:3">行業新聞</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="setting" />工程業績
+              </span>
+            }
+          >
+            <Menu.Item key="setting:1">華東地區</Menu.Item>
+            <Menu.Item key="setting:2">華南地區</Menu.Item>
+            <Menu.Item key="setting:3">華中地區</Menu.Item>
+            <Menu.Item key="setting:4">西南地區</Menu.Item>
+            <Menu.Item key="setting:5">華北地區</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="setting" />技術研發
+              </span>
+            }
+          >
+            <Menu.Item key="setting:1">核心技術</Menu.Item>
+            <Menu.Item key="setting:2">知識產權</Menu.Item>
+            <Menu.Item key="setting:3">產學研合作</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="setting" />交流合作
+              </span>
+            }
+          >
+            <Menu.Item key="setting:1">大陸合作</Menu.Item>
+            <Menu.Item key="setting:2">港臺合作</Menu.Item>
+            <Menu.Item key="setting:3">國際合作</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="setting" />招賢納士
+              </span>
+            }
+          >
+            <Menu.Item key="setting:1">領軍人才</Menu.Item>
+            <Menu.Item key="setting:2">招賢納士</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="setting" />聯係我們
+              </span>
+            }
+          >
+            <Menu.Item key="setting:1">聯係我們</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="setting" />新聞中心
+              </span>
+            }
+          >
+            <Menu.Item key="setting:1">Option 1</Menu.Item>
+            <Menu.Item key="setting:2">Option 2</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Header>
+    );
+  }
 }
 
 const state = {
-    current: 'mail'
-}
-
+  current: 'mail'
+};
