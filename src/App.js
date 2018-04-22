f76c7d5be856d6af11604222d2f82c63d4b66c00
poppintk <youdonghoho@hotmail.com>
2018-04-21 22:37:24 -0400

    start new

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React,{ Component } from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Main from './content/Main';
-import AboutUs from './content/aboutus';
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
-export default class App extends Component {
-  render(){
-    return(
-      <div>
-        <Header/>
-        <AboutUs/>
-        {/* footer */}
-        <Footer/>
-      </div>
+class App extends Component {
+  render() {
+    return (
+      <Layout className="layout">
+          <div className="logo" />
+          <Menu
+            theme="white"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+
+          <Content style={{height:'calc(100vh - 107px)'}}>
+
+          </Content>
+          <Footer style={{ textAlign: 'center' ,backgroundColor:'#001529',color:'white'}}>
+            Ant Design ©2016 Created by Ant UED
+          </Footer>
+        </Layout>
+       
     );
   }
 }
+
+export default App;
