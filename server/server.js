222a6bec1ae8807a90d59947c2ecf4bf8a54005d
Ryou <youdonghoho@hotmail.com>
2018-10-21 12:29:29 -0400

    u

--- server/server.js
+++ server/server.js
@@ -5,40 +5,40 @@ const bodyParser = require('body-parser')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
 
 const mailer = require('./mailer')
 
+const server = express()
 
-app.prepare().then(() => {
-  const server = express()
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-  server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+server.use(bodyParser.json())
 
-  server.use(bodyParser.json())
+server.get("*", (req, res) => {
+  res.send("hello");
+});
 
-  server.post('/api/contact', (req, res) => {
-    let values = req.body;
-    process.stdout.write(values + '\n');
-    const { email = '', body = '', phone='',subject='' } = values;
+server.post('/api/contact', (req, res) => {
+  let values = req.body;
+  process.stdout.write(values + '\n');
+  const { email = '', body = '', phone='',subject='' } = values;
 
 
-    mailer({ email, phone, text: body,subject }).then(() => {
-      console.log('success')
-      res.send('success')
-    }).catch((error) => {
-      console.log('failed', error)
-      res.send('badddd')
-    })
+  mailer({ email, phone, text: body,subject }).then(() => {
+    console.log('success')
+    res.send('success')
+  }).catch((error) => {
+    console.log('failed', error)
+    res.send('badddd')
   })
+})
 
 
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Read on http://localhost:3000')
-  })
+server.listen(3000, (err) => {
+  if (err) throw err
+  console.log('> Read on http://localhost:3000')
 })
