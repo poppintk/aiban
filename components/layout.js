4033134d07d7765bfa8ab697f4f644aa7a09a837
poppintk <youdonghoho@hotmail.com>
2019-08-13 22:00:50 -0400

    update

--- components/layout.js
+++ components/layout.js
@@ -1,27 +1,27 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import React from 'react';
-import Hover from './hover';
-import { Motion, spring } from 'react-motion';
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import Hover from "./hover";
+import { Motion, spring } from "react-motion";
 import { Menu, Icon, Button } from "antd";
 const SubMenu = Menu.SubMenu;
 
 export default class Layout extends React.Component {
   componentDidMount() {
-    var bp = document.createElement('script');
-    var curProtocol = window.location.protocol.split(':')[0];
-    if (curProtocol === 'https') {
-      bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    var bp = document.createElement("script");
+    var curProtocol = window.location.protocol.split(":")[0];
+    if (curProtocol === "https") {
+      bp.src = "https://zz.bdstatic.com/linksubmit/push.js";
     } else {
-      bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+      bp.src = "http://push.zhanzhang.baidu.com/push.js";
     }
-    var s = document.getElementsByTagName('script')[0];
+    var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
   }
   render() {
-    const english_text = { fontSize: '9px' };
+    const english_text = { fontSize: "9px" };
     return (
-      <div style={{ overflow: 'hidden' }}>
+      <div style={{ overflow: "hidden" }}>
         <Head>
           <title>湖南爱邦正明环保工程公司</title>
           <meta
@@ -70,13 +70,15 @@ export default class Layout extends React.Component {
             crossOrigin="anonymous"
           />
 
-          <link rel='stylesheet' href='http://cdn.bootcss.com/antd/2.9.3/antd.css' />
-
+          <link
+            rel="stylesheet"
+            href="http://cdn.bootcss.com/antd/2.9.3/antd.css"
+          />
         </Head>
         <style jsx>{`
           * {
-            font-family: 'SF Pro Display', Roboto, Noto, Arial, 'PingFang SC',
-              'Hiragino Sans GB', 'Microsoft YaHei', sans-serif;
+            font-family: "SF Pro Display", Roboto, Noto, Arial, "PingFang SC",
+              "Hiragino Sans GB", "Microsoft YaHei", sans-serif;
           }
           a {
             line-height: 100%;
@@ -89,41 +91,39 @@ export default class Layout extends React.Component {
         <div
           className="container-fluid"
           style={{
-            backgroundColor: 'rgb(247, 250, 255)',
-            color: 'black',
-            fontSize: '12px',
-            position: 'fixed',
-            zIndex: '90',
+            backgroundColor: "rgb(247, 250, 255)",
+            color: "black",
+            fontSize: "12px",
+            position: "fixed",
+            zIndex: "90",
             backgroundImage:
-              'url(https://aiban.oss-cn-beijing.aliyuncs.com/banner2.jpg)',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center center',
+              "url(https://aiban.oss-cn-beijing.aliyuncs.com/banner2.jpg)",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center"
           }}
         >
           <div className="form-inline">
             <marquee
               style={{
-                width: 'calc(100vw - 260px)',
-                padding: '10px',
-                fontSize: '12px',
-                color: 'black',
-                fontWeight:'bold' 
+                width: "calc(100vw - 260px)",
+                padding: "10px",
+                fontSize: "12px",
+                color: "black",
+                fontWeight: "bold"
               }}
             >
               爱邦兴国 博爱明慧 爱邦正明 环保先锋 让整个地球共享一片蓝天
             </marquee>
             <div
               style={{
-                width: '210px',
-                color: 'black',
-                fontWeight:'bold' 
+                width: "210px",
+                color: "black",
+                fontWeight: "bold"
               }}
             >
-              业务咨询电话：<a
-                style={{ color: 'black'}}
-                href="tel:0086-183-9098-0101"
-              >
+              业务咨询电话：
+              <a style={{ color: "black" }} href="tel:0086-183-9098-0101">
                 0086-183-9098-0101
               </a>
             </div>
@@ -132,34 +132,34 @@ export default class Layout extends React.Component {
         <nav
           className="navbar navbar-expand-lg navbar-light"
           style={{
-            position: 'relative',
-            top: '0px',
-            backgroundColor: '#edf3ff',
+            position: "relative",
+            top: "0px",
+            backgroundColor: "#edf3ff",
             backgroundImage:
-              'url(https://aiban.oss-cn-beijing.aliyuncs.com/banner.jpeg)',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center center',
-            zIndex: '99',
+              "url(https://aiban.oss-cn-beijing.aliyuncs.com/banner.jpeg)",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            zIndex: "99"
           }}
         >
-          <a className="navbar-brand" href="/" style={{ display: 'flex' }}>
+          <a className="navbar-brand" href="/" style={{ display: "flex" }}>
             <img
               src="https://aiban.oss-cn-beijing.aliyuncs.com/logo2.png"
               style={{
-                width: '60px',
-                height: '50px',
+                width: "60px",
+                height: "50px"
               }}
             />
 
             <div
               style={{
                 fontSize: 18,
-                fontWeight: 'bold',
-                marginLeft: '10px',
-                lineHeight: '100%',
-                marginTop: '5px',
-                color: 'black',
+                fontWeight: "bold",
+                marginLeft: "10px",
+                lineHeight: "100%",
+                marginTop: "5px",
+                color: "black"
               }}
             >
               爱邦正明环保
@@ -182,69 +182,69 @@ export default class Layout extends React.Component {
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
                 <Link prefetch href="/">
-                  <a className="nav-link" style={{ color: 'black' }}>
+                  <a className="nav-link" style={{ color: "black" }}>
                     <Hover
-                      chineseTitle={'首页'}
-                      englishTitle={'HOME'}
+                      chineseTitle={"首页"}
+                      englishTitle={"HOME"}
                       submenu={[]}
                     />
                   </a>
                 </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" style={{ color: 'black' }}>
+                <a className="nav-link" style={{ color: "black" }}>
                   <Hover
-                    chineseTitle={'关于我们'}
-                    englishTitle={'ABOUT US'}
+                    chineseTitle={"关于我们"}
+                    englishTitle={"ABOUT US"}
                     submenu={[
-                      { name: '企业文化', url: '/culture' },
-                      { name: '关于我们', url: '/about' },
-                      { name: '企业荣耀', url: '/honor' },
+                      { name: "企业文化", url: "/culture" },
+                      { name: "关于我们", url: "/about" },
+                      { name: "企业荣耀", url: "/honor" }
                     ]}
                   />
                 </a>
               </li>
               <li className="nav-item">
                 <Link prefetch href="/news">
-                  <a className="nav-link" style={{ color: 'black' }}>
+                  <a className="nav-link" style={{ color: "black" }}>
                     <Hover
-                      chineseTitle={'新闻中心'}
-                      englishTitle={'NEWS'}
+                      chineseTitle={"新闻中心"}
+                      englishTitle={"NEWS"}
                       submenu={[]}
                     />
                   </a>
                 </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" style={{ color: 'black' }}>
+                <a className="nav-link" style={{ color: "black" }}>
                   <Hover
-                    chineseTitle={'业绩展示'}
-                    englishTitle={'PROJECTS'}
+                    chineseTitle={"业绩展示"}
+                    englishTitle={"PROJECTS"}
                     submenu={[
-                      { name: '业绩展示', url: '/service' },
-                      { name: '工程实例', url: '/case' },
+                      { name: "业绩展示", url: "/service" },
+                      { name: "工程实例", url: "/case" }
                     ]}
                   />
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" style={{ color: 'black' }}>
+                <a className="nav-link" style={{ color: "black" }}>
                   <Hover
-                    chineseTitle={'工程实例'}
-                    englishTitle={'CASES'}
+                    chineseTitle={"工程实例"}
+                    englishTitle={"CASES"}
                     submenu={[
-                      { name: '工程实例', url: '/case' },
-                      { name: '业绩展示', url: '/service' },
+                      { name: "工程实例", url: "/case" },
+                      { name: "业绩展示", url: "/service" }
                     ]}
                   />
                 </a>
               </li>
               <li className="nav-item">
                 <Link prefetch href="/tech">
-                  <a className="nav-link" style={{ color: 'black' }}>
+                  <a className="nav-link" style={{ color: "black" }}>
                     <Hover
-                      chineseTitle={'技术研发'}
-                      englishTitle={'R&D'}
+                      chineseTitle={"技术研发"}
+                      englishTitle={"R&D"}
                       submenu={[]}
                     />
                   </a>
@@ -252,10 +252,10 @@ export default class Layout extends React.Component {
               </li>
               <li className="nav-item">
                 <Link prefetch href="/coop">
-                  <a className="nav-link" style={{ color: 'black' }}>
+                  <a className="nav-link" style={{ color: "black" }}>
                     <Hover
-                      chineseTitle={'交流合作'}
-                      englishTitle={'COOPERATION'}
+                      chineseTitle={"交流合作"}
+                      englishTitle={"COOPERATION"}
                       submenu={[]}
                     />
                   </a>
@@ -263,36 +263,36 @@ export default class Layout extends React.Component {
               </li>
               <li className="nav-item">
                 <Link prefetch href="/team">
-                  <a className="nav-link" style={{ color: 'black' }}>
+                  <a className="nav-link" style={{ color: "black" }}>
                     <Hover
-                      chineseTitle={'领军人才'}
-                      englishTitle={'TEAM'}
+                      chineseTitle={"领军人才"}
+                      englishTitle={"TEAM"}
                       submenu={[]}
                     />
                   </a>
                 </Link>
               </li>
               <li className="nav-item">
-                  <a className="nav-link" style={{ color: 'black' }}>
-                    <Hover
-                    chineseTitle={'联系我們'}
-                    englishTitle={'CONTACT US'}
+                <a className="nav-link" style={{ color: "black" }}>
+                  <Hover
+                    chineseTitle={"联系我們"}
+                    englishTitle={"CONTACT US"}
                     submenu={[
-                      { name: '联系方式', url: '/contact' },
-                      { name: '发送邮件', url: '/qa' }
+                      { name: "联系方式", url: "/contact" },
+                      { name: "发送邮件", url: "/qa" }
                     ]}
                   />
-                  </a>
+                </a>
               </li>
             </ul>
           </div>
         </nav>
         <Motion defaultStyle={{ offset: 2 }} style={{ offset: spring(2) }}>
-          {(style) => {
+          {style => {
             return (
               <div
                 style={{
-                  marginTop: style.offset,
+                  marginTop: style.offset
                 }}
               >
                 {this.props.children}
@@ -300,22 +300,21 @@ export default class Layout extends React.Component {
             );
           }}
         </Motion>
-        <div style={{position:'fixed',bottom:"81px"}}>
+        <div style={{ position: "fixed", bottom: "81px" }}>
           <Menu
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
             mode="inline"
             theme="white"
             inlineCollapsed={true}
-            onClick={(submenu)=>{
-              if(submenu.key == 1){
-                window.location.href = '/contact';
-              }else if(submenu.key == 2){
-                window.location.href = '/qa';
+            onClick={submenu => {
+              if (submenu.key == 1) {
+                window.location.href = "/contact";
+              } else if (submenu.key == 2) {
+                window.location.href = "/qa";
               }
-
             }}
-            style={{zIndex:999}}
+            style={{ zIndex: 999 }}
           >
             <Menu.Item key="1">
               <Icon type="message" theme="outlined" />
@@ -329,58 +328,57 @@ export default class Layout extends React.Component {
               <Icon type="mail" theme="outlined" />
               <span>发送邮件</span>
             </Menu.Item>
-
           </Menu>
         </div>
         <footer
           className="container-fluid"
           style={{
-            backgroundColor: 'rgb(247, 250, 255,0.9)',
-            float: 'bottom',
+            backgroundColor: "rgb(247, 250, 255,0.9)",
+            float: "bottom",
             backgroundImage:
-              'url(https://aiban.oss-cn-beijing.aliyuncs.com/company_banner.jpg)',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center center',
-            fontWeight: 'bold',
-            transform: 'scaleY(-1)',
+              "url(https://aiban.oss-cn-beijing.aliyuncs.com/company_banner.jpg)",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            fontWeight: "bold",
+            transform: "scaleY(-1)"
           }}
         >
-          <div style={{ transform: 'scaleY(-1)' }}>
+          <div style={{ transform: "scaleY(-1)" }}>
             <p className="float-right">
               <a href="#">返回顶部</a>
             </p>
-            <div style={{ clear: 'both' }}>
-              <div style={{ textAlign: 'center', height: '35px' }}>
-                版权所有：湖南爱邦正明环保工程公司
-                备案号：湘备ICP1500
+            <div style={{ clear: "both" }}>
+              <div style={{ textAlign: "center", height: "35px" }}>
+                版权所有：湖南爱邦正明环保工程公司 备案号：湘ICP备15004135号-1
               </div>
+
               <div
                 style={{
-                  color: 'black',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  marginTop: '-12px',
-                  height: '40px',
-                  paddingBottom: '15px',
+                  color: "black",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginTop: "-12px",
+                  height: "40px",
+                  paddingBottom: "15px"
                 }}
               >
-                <div style={{ marginLeft: '10px' }}>
+                <div style={{ marginLeft: "10px" }}>
                   <a
                     href="mailto:1317426180@qq.com"
-                    style={{ color: 'black', textDecoration: 'none' }}
+                    style={{ color: "black", textDecoration: "none" }}
                   >
                     企业邮箱
                   </a>
                 </div>
                 {/* <div style={{ marginLeft: '10px' }}>网站地图 </div> */}
-                <div style={{ marginLeft: '10px' }}>友情链接：</div>
+                <div style={{ marginLeft: "10px" }}>友情链接：</div>
                 <select
                   defaultValue="国际环保城"
                   style={{ width: 120 }}
-                  onChange={(e) => onChangeHandler(e)}
-                  size={'small'}
+                  onChange={e => onChangeHandler(e)}
+                  size={"small"}
                 >
                   <option value="http://www.iepz.cc/">国际环保城</option>
                   <option value="http://green.sina.com.cn/">新浪环保</option>
@@ -406,6 +404,6 @@ export default class Layout extends React.Component {
   }
 }
 
-const onChangeHandler = (e) => {
-  window.open(e.currentTarget.value, '_blank');
+const onChangeHandler = e => {
+  window.open(e.currentTarget.value, "_blank");
 };
