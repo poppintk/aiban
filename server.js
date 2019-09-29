732ef44483f958de9f340e24ef4702cd5488e0fe
poppintk <youdonghoho@hotmail.com>
2019-09-29 09:24:29 -0400

    update

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("*", (req, res) => {
+    if (req.get("host") == "abhb.com.cn") {
+      res.status(301).redirect("http://www.hnabzm.com");
+    }
+    return handle(req, res);
+  });
+
+  server.listen(80, err => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:80");
+  });
+});
